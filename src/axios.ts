a3613226e79bcde963f75ad430f89ee9dd258fad
mgl <1375022007@qq.com>
2020-04-12 10:49:14 +0800

    feat(src/*): 对axios请求进行增强，扩展用法

--- src/axios.ts
+++ src/axios.ts
@@ -1,39 +1,14 @@
-import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from './types'
-import xhr from './xhr'
-import { bulidURL } from './helpers/url'
-import { transformRequest, transformReponse } from './helpers/data'
-import { processHeaders } from './helpers/header'
+import { AxiosInstance } from './types'
+import Axios from './core/Axios'
+import { extend } from './helpers/util'
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then((res: any) => {
-    return transformResponseData(res)
-  })
+function createInstance(): AxiosInstance {
+  const context = new Axios()
+  const instance = Axios.prototype.request.bind(context)
+  extend(instance, context)
+  return instance as AxiosInstance
 }
 
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformReponse(res.data)
-  return res
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return bulidURL(url, params)
-}
+const axios = createInstance()
 
 export default axios
