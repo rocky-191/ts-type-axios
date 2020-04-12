66b03617efbe1be3e99043f13c7c50f7b93770ae
mgl <1375022007@qq.com>
2020-04-12 11:39:39 +0800

    feat(core/Axios): 实现axios函数重载

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -8,6 +8,14 @@ axios({
   }
 })
 
+axios('/extend/post',{
+    method:'post',
+    data:{
+      msg:'hello'
+    }
+  }
+)
+
 axios.request({
   url:'/extend/post',
   method:'post',
@@ -18,14 +26,14 @@ axios.request({
 
 axios.get('/extend/get')
 
-axios.options('/extend/options')
+// axios.options('/extend/options')
 
 axios.delete('/extend/delete')
 
-axios.head('/extend/head')
+// axios.head('/extend/head')
 
 axios.post('/extend/post', { msg: 'post' })
 
 axios.put('/extend/put', { msg: 'put' })
 
-axios.patch('/extend/patch', { msg: 'patch' })
+// axios.patch('/extend/patch', { msg: 'patch' })
