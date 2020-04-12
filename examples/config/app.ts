237929befa4499f8acd49ec539f29725c6df6ebe
mgl <1375022007@qq.com>
2020-04-12 20:51:48 +0800

    chore(examples): 扩展axios.create静态接口实例

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,19 +1,31 @@
-import axios from '../../src/index'
+import axios, {AxiosTransformer} from '../../src/index'
 import qs from 'qs'
 
-axios.defaults.headers.common['test2'] = 123
-
+const instance = axios.create({
+  transformRequest: [
+    (function (data) {
+      return qs.stringify(data);
+    }),
+    ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]),
+    function (data) {
+      if (typeof data === 'object') {
+        data.b = 2;
+      }
+      return data;
+    }
+  ],
+});
 
 // tslint:disable-next-line: no-floating-promises
-axios({
+instance({
   url: '/config/post',
   method: 'post',
-  data: qs.stringify({
+  data: {
     a: 1
-  }),
-  headers: {
-    test: '321'
   }
-}).then((res) => {
-  console.log(res.data)
-})
+}).then(response => {
+  console.log(response.data);
+});
